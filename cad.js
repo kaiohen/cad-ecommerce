ae4866654f94eb7c53f9e3e93be1e10ad80be683
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-10-18 08:47:20 -0300

    validação do cpf

--- cad.js
+++ cad.js
@@ -64,3 +64,39 @@ function verificarCampos() {
   };
   //adiciona escutador para executar consumo de API da ViaCep
   document.getElementById("consultarCep").addEventListener("click", pesquisarCep);
+  // VALIDAÇÃO DE CPF
+  function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, ""); // remove caracteres não numéricos
+    // verificar se o valor informado contem 11 digitos e se todos são digitos iguais
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+      return false;
+    }
+    let soma = 0;
+    let resto;
+    // validação do primeiro digito verificador
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+      return false;
+    }
+    soma = 0;
+    // validar 11 elementos do CPF - 2º digito verificador
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+   
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+      return false;
+    }
+    return true;
+  }
+  
